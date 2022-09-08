c7d681ea89baeb1fe29260424591597eb55f3954
cyrusdeleon <95203865+cyrusdeleon@users.noreply.github.com>
2022-09-08 11:19:38 +0800

    Add files via upload

---
+++ Ex02MuonDeLeon.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex02muondeleon;
+
+/**
+ *
+ * @author MUON
+ */
+public class Ex02MuonDeLeon {
+
+    public static void main(String[] args) {
+        System.out.println ("Welcome to Higher or Lower! What will you do?");
+        System.out.println ("-Start Game");
+        System.out.println("-Change Settings");
+        System.out.println ("-End Application");
+        
+    }
+    
+}
